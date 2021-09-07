341397121370acb5bc87e7f4b83046ca7aad645f
lolo <lolozhanghq@gmail.com>
2021-09-06 21:07:40 -0700

    learning gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+const { series } = require("gulp");
+var gulp = require("gulp")
+var sass = require("gulp-sass")
+
+
+var runSass = function () {
+  // we want to run "sass css/app/scss app.css --watch"
+  return gulp.src("css/app.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("."));
+}
+
+var watchSass = function () {
+  gulp.watch("css/app.scss", (runSass))
+}
+
+
+exports.sass = runSass
+exports.default = series (runSass, watchSass)
+exports.watch = watchSass
